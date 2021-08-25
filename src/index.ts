a316d11aaa6cb2720a9f75beec46507c1b88692c
danielromeo <danielromeo99@gmail.com>
2021-08-25 20:33:41 +0200

    ready for release

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
 export * from './coordinate.js'
 export * from './functions.js';
-
-import {Coordinate} from './coordinate.js';
-import {Methods} from './functions.js';
-import { isAntiSymmetric, isIrreflexive, isReflexive, isSymmetric, isTransitive } from './properties-relations.js';
